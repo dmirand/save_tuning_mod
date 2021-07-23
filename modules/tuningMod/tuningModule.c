b4a9bf45a751fbc938d7437d521546028921e4f1
root <root@dm-linux.attlocal.net>
2021-07-23 17:08:17 -0400

    created kthreads

--- modules/tuningMod/tuningModule.c
+++ modules/tuningMod/tuningModule.c
@@ -3,10 +3,41 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/completion.h>
+#include <linux/kthread.h>
+#include <linux/errno.h>
 
 MODULE_LICENSE("Tuning BSD/GPL");
 
 #define DEVICE_NAME	"tuningMod"
+#define ISSUE_WARNING \
+   printk(  "<1>\nWarning From " ) ; \
+   printk(  __FILE__ ) ;\
+   printk(  "<1> " ) ;\
+   printk(  __FUNCTION__ ) ;\
+   printk(  "<1>() " ) ; \
+   printk(  "<1>Line# %d\n", __LINE__ ) ;\
+   printk(  "<1>" ) ;\
+   printk(
+
+// 50 jiffies = 200 msecs
+#define WAIT_TIMEOUT    HZ/5
+
+void TuningModule__Wrapper_Sleep( unsigned long duration )
+{
+  DECLARE_WAIT_QUEUE_HEAD(wq);
+  wait_event_timeout(wq, 0, duration);
+}
+
+DECLARE_COMPLETION(RFU_side_completion);
+DECLARE_WAIT_QUEUE_HEAD(RFU_Thread_Queue);
+static int RFU_side_stop_flag = 0;
+atomic_t RFU_Thread_Queue_Flag = ATOMIC_INIT(0);
+
+DECLARE_COMPLETION(RFC_side_completion);
+DECLARE_WAIT_QUEUE_HEAD(RFC_Thread_Queue);
+static int RFC_side_stop_flag = 0;
+atomic_t RFC_Thread_Queue_Flag = ATOMIC_INIT(0);
 
 static int tuning_open(struct inode*, struct file*);
 static int tuning_release(struct inode*, struct file*);
@@ -21,6 +52,83 @@ static struct file_operations fops = {
 };
 
 static int major;
+
+// 50 jiffies = 200 msecs
+#define WAIT_TIMEOUT    HZ/5
+
+void TuningModule_Wrapper_Sleep( unsigned long duration )
+{
+  DECLARE_WAIT_QUEUE_HEAD(wq);
+  wait_event_timeout(wq, 0, duration);
+}
+
+int Process_Event_From_Userspace(void){
+	static unsigned long count = 0;
+
+	TuningModule_Wrapper_Sleep((unsigned long)2500);
+	printk(KERN_ALERT "***In process event from Userspace*** %ld\n", count);
+
+	return 0;
+}
+
+int Process_Event_From_Collector(void){
+	static unsigned long count = 0;
+
+	TuningModule_Wrapper_Sleep((unsigned long)2000);
+	printk(KERN_ALERT "***In process event from Collector*** %ld\n", count);
+
+	return 0;
+}
+
+/*------------------------------------------------
+d) ;
+------------------------------------------------*/
+static struct task_struct *rfu_Thread_task  ;
+static int Read_From_Userspace_Thread_Loop( void *p )
+{
+int j;
+
+   ISSUE_WARNING "Read_From_Userspace_Thread_Loop) is being started.\n" ) ;
+
+
+   while( 1 ){
+
+      wait_event_timeout(RFU_Thread_Queue, atomic_dec_and_test(&RFU_Thread_Queue_Flag), WAIT_TIMEOUT);
+
+        j = Process_Event_From_Userspace() ;
+        //atomic_set(&RFU_Thread_Queue_Flag,1);
+      if( RFU_side_stop_flag )
+         break ;
+   };
+   
+	complete_and_exit(&RFU_side_completion, 0);
+   	return 0;
+}
+	
+/*------------------------------------------------
+Read_From_Collector_Thread_Loop(void * p) ;
+------------------------------------------------*/
+static struct task_struct *rfc_Thread_task  ;
+static int Read_From_Collector_Thread_Loop( void *p )
+{
+int j;
+
+   ISSUE_WARNING "Read_From_Userspace_Thread_Loop) is being started.\n" ) ;
+
+
+   while( 1 ){
+
+      wait_event_timeout(RFC_Thread_Queue, atomic_dec_and_test(&RFC_Thread_Queue_Flag), WAIT_TIMEOUT);
+
+        j = Process_Event_From_Collector() ;
+        //atomic_set(&RFU_Thread_Queue_Flag,1);
+      if( RFC_side_stop_flag )
+         break ;
+   };
+   
+	complete_and_exit(&RFC_side_completion, 0);
+   	return 0;
+}
 	
 static int tuningModule_init(void) {
 	major = register_chrdev(0, DEVICE_NAME, &fops);
@@ -32,12 +140,31 @@ static int tuningModule_init(void) {
 	}
 
 	printk(KERN_INFO "tuningModule has been loaded %d\n", major);
+
+      rfu_Thread_task = kthread_run( Read_From_Userspace_Thread_Loop, NULL, "read_From_Userspace_Thread_Loop");
+      if (IS_ERR(rfu_Thread_task) ){
+        return PTR_ERR(rfu_Thread_task ) ;
+      };
+      
+	  rfc_Thread_task = kthread_run( Read_From_Collector_Thread_Loop, NULL, "read_From_Collector_Thread_Loop");
+      if (IS_ERR(rfc_Thread_task) ){
+        return PTR_ERR(rfc_Thread_task ) ;
+      };
+
+
 	return 0;
 }
 
 static void tuningModule_exit(void) {
+	ISSUE_WARNING "TuningModule is being Removed\n");
+
+	RFU_side_stop_flag  = 1   ;
+	wait_for_completion(&RFU_side_completion);
+	
+	RFC_side_stop_flag  = 1   ;
+	wait_for_completion(&RFC_side_completion);
 	unregister_chrdev(major, DEVICE_NAME);
-	printk(KERN_INFO "tuningModule has been unloaded\n");
+
 	return;
 }
 
