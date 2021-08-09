8675af787b672b75d220836d5d7ebbde162b49a0
root <root@dm-linux.attlocal.net>
2021-08-09 15:08:00 -0400

    changes to userspace

--- userspace/gdv.sh
+++ userspace/gdv.sh
@@ -1,12 +1,12 @@
 
-sysctl net.core.rmem_max >> /tmp/default_sysctl_config
+sysctl net.core.rmem_max > /tmp/default_sysctl_config
 sysctl net.core.wmem_max >> /tmp/default_sysctl_config
 
 sysctl net.ipv4.tcp_rmem >> /tmp/default_sysctl_config
 sysctl net.ipv4.tcp_wmem >> /tmp/default_sysctl_config
-sysctl net.ipv4.tcp_mem >> /tmp/default_sysctl_config
+#sysctl net.ipv4.tcp_mem >> /tmp/default_sysctl_config
 
-sysctl net.ipv4.tcp_available_congestion_control >> /tmp/default_sysctl_config
+#sysctl net.ipv4.tcp_available_congestion_control >> /tmp/default_sysctl_config
 sysctl net.ipv4.tcp_congestion_control >> /tmp/default_sysctl_config
 
 sysctl net.ipv4.tcp_mtu_probing >> /tmp/default_sysctl_config
--- userspace/user_dtn.c
+++ userspace/user_dtn.c
@@ -37,14 +37,16 @@ typedef struct {
  * Increase rmem_max and wmem_max so they are at least as large as the third 
  * values of tcp_rmem and tcp_wmem.
  */
+#define TUNING_NUMS	7
+/* Must change TUNING_NUMS if adding more to the array below */
 
-host_tuning_vals_t aTuningNumsToUse[] = {
+host_tuning_vals_t aTuningNumsToUse[TUNING_NUMS] = {
     {"net.core.rmem_max",   			67108864,       	0,      	0},
-    {"net.core.rmem_max",   			67108864,       	0,      	0},
+    {"net.core.wmem_max",   			67108864,       	0,      	0},
     {"net.ipv4.tcp_rmem",       			4096,       87380,   33554432},
     {"net.ipv4.tcp_wmem",       			4096,       65536,   33554432},
     {"net.ipv4.tcp_mtu_probing",			   1,       	0,      	0},
-    {"net.ipv4.tcp_congestion_control",	   cubic, 			0, 			0}, //uses #defines
+    {"net.ipv4.tcp_congestion_control",	   cubic, 			0, 			0}, //uses #defines to help
     {"net.core.default_qdisc",fq_codel, 0, 0}, //uses #defines
 };
 
@@ -54,20 +56,54 @@ void fDoSystemTuning(void)
 	char *line = NULL;
     size_t len = 0;
     ssize_t nread;
+	char *q, *r, *p = 0;
+	char setting[256];
+	char values[256];
+	int count, found = 0;
+	
 
     while ((nread = getline(&line, &len, tunDefSysCfgPtr)) != -1) {
-    	printf("Retrieved line of length %zu:\n", nread);
-		printf("&%s&",line);
-     //          fwrite(line, nread, 1, stdout);
-           }
+    	//printf("Retrieved line of length %zu:\n", nread);
+		//printf("&%s&",line);
+		p = line;
+		q = strchr(line,' '); //search for space	
+		len = (q-p) + 1;
+		strncpy(setting,p,len);
+		if (setting[len-1] == ' ')
+			setting[--len] = 0;
+		else
+			setting[len] = 0;
 
+		printf("\nsetting is ***%s***\n",setting);
+		/* compare with known list now */
+		for (count = 0; count < TUNING_NUMS; count++)
+		{
+			if (strcmp(aTuningNumsToUse[count].setting, setting) == 0) //found
+			{
+				q++;// move it up past the space
+				q = strchr(q,' '); //search for next space	
+				q++; // move to the beginning of 1st (maybe only) number
+				r = strchr(q,'\n'); //search for newline
+				len = (r-q) + 1;
+							
+				printf("Minimum of %s is *%d*\n",setting, aTuningNumsToUse[count].minimum);
+
+			}
+				
+		}
+
+		if (!found)
+			printf("ERR*** Could not find the following setting **%s**\n", setting);
+		
+	}
 
 	fprintf(tunLogPtr, "Closing Tuning Module default system configuration file***\n");
 	fclose(tunDefSysCfgPtr);
-if(line)
-free(line);
 
-return;
+	if(line)
+		free(line);
+
+	return;
 }
 
 int main() 
