a1654ec39fab7604ef58125fd62a403998d98fec
David Miranda <dm33187@hotmail.com>
2021-11-10 21:58:09 +0000

    fixup packaging for dtn_tune

--- packaging/createpkg.sh
+++ packaging/createpkg.sh
@@ -1,4 +1,6 @@
 cp ../assess-tuning/dtn_tune .
+cp ../assess-tuning/dtn_menu .
+cp ../assess-tuning/dtn_menu.sh .
 cp ../userspace/gdv.sh .
 zip dtntune.zip dtn_tune gdv.sh user_config.txt dtn_menu.sh dtnmenu readme.txt
 
