8e7688af2ea675aea5225275819cfbc999bb286d
David Miranda <dm33187@hotmail.com>
2021-12-07 16:28:39 +0000

    move changes from assess portion to Tuning Module

--- userspace/gdv.sh
+++ userspace/gdv.sh
@@ -2,7 +2,8 @@
 sysctl net.core.rmem_max > /tmp/current_config.orig
 sysctl net.core.wmem_max >> /tmp/current_config.orig
 
-#sysctl net.ipv4.tcp_available_congestion_control >> /tmp/current_config.orig
+#net.ipv4.tcp_available_congestion_control *MUST be checked before* net.ipv4.tcp_congestion_control
+sysctl net.ipv4.tcp_available_congestion_control >> /tmp/current_config.orig
 sysctl net.ipv4.tcp_congestion_control >> /tmp/current_config.orig
 
 sysctl net.ipv4.tcp_mtu_probing >> /tmp/current_config.orig
@@ -10,5 +11,4 @@ sysctl net.core.default_qdisc >> /tmp/current_config.orig
 
 sysctl net.ipv4.tcp_rmem >> /tmp/current_config.orig
 sysctl net.ipv4.tcp_wmem >> /tmp/current_config.orig
-#sysctl net.ipv4.tcp_mem >> /tmp/current_config.orig
 
